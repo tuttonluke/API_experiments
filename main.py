db422e0cfa3011901643aca76ed98f52beac6a79
Luke Tutton <tuttonluke@gmail.com>
2023-03-27 14:38:27 +0100

    Merge branches.

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Path operation function
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "Welcome to my API"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data" : "These are your posts."}
