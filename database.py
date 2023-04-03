58a25038bb97b4e7a57fd80702b6d5db5b63d233
Luke Tutton <tuttonluke@gmail.com>
2023-04-03 10:47:41 +0100

    Adds comments.

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import sqlalchemy as sql
-import sqlalchemy.ext.declarative as declarative
-import sqlalchemy.orm as orm
+import sqlalchemy.ext.declarative as declarative 
+import sqlalchemy.orm as orm # object-relational mapping - convert data between a relational database and the heap of an object-oriented programming language   
 
 DATABASE_URL = "postgresql://myuser:password@localhost/fast_api_database"
 
@@ -8,4 +8,4 @@ enigne = sql.create_engine(DATABASE_URL)
 
 SessionLocal = orm.sessionmaker(autocommit=False, autoflush=False, bind=enigne)
 
-Base = declarative.declarative_base()
+Base = declarative.declarative_base() # base class from which to inherit
